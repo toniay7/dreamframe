42ac6c6d94b04c36c7b707e9d73f11c598429aa5
Your Name <youremail@example.com>
2023-09-17 12:11:17 +0200

    Update Flask app to bind to the correct port on Heroku

--- backend_code.py
+++ backend_code.py
@@ -63,4 +63,6 @@ def process_image():
     return jsonify({"finalImageUrl": finalImageUrl})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
